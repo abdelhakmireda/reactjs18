cf95d5f7333e83a8fd7650b3c2c9f289a3e2de89
abdelhakmireda <abdelhakmreda37@gmail.com>
2023-07-10 23:46:50 +0100

    props

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import {Header , Footer , Container} from './components/index'
+
+const App = () => {
+  return (
+    <>
+    <Header text="reda oupsi" color ="blue"/>
+    <Container />
+    <Footer />
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import './Header.css'
+
+const Header = (props) => {
+  return (
+    <div class="navbar navbar-expand-md bg-dark navbar-dark test-white fixed-top">
+    <div class="container">
+    <a href="#" class="navbar-brand">Bootstrap Tutorial</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mainmenu">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a href="#hero" class="nav-link">Get Started</a></li>
+            <li class="nav-item"><a href="#features" class="nav-link">Features</a></li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Learn</a>
+                <ul class="dropdown-menu">
+                    <li><a href="#learn" class="dropdown-item">Learn Bootstrap</a></li>
+                    <li><a href="#next" class="dropdown-item">Where to go next</a></li>
+                </ul>
+            </li>
+            <li class="nav-item"><a href="#faq" class="nav-link">Ask Me</a></li>
+            <li class="nav-item"><a href="./index-ar.html" class="nav-link">عربي</a></li>
+        </ul>
+    </div>
+</div>
+</div>
+  )
+}
+
+export default Header
